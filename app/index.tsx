e62c044fa05a7b8fcdc3b33b60f8aa41d433bdfc
vaishnavi01122006 <vaishnaviparulekar0@gmail.com>
2026-08-12 19:26:20 +0530

    Initial commit - pathology app frontend

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Image
-        source={require("../../assets/images/logo.png")}
+        source={require("../assets/images/logo.png")}
         style={styles.logo}
         resizeMode="contain"
       />
