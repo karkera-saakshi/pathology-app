84210ab1f9031312a8132fee34834cfe2862e6d8
Jiya-Nair <jiyahnair2020@gmail.com>
2026-09-18 13:45:47 +0530

    clean up duplicate code

--- backend/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-// utils/generateToken.js
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-
-module.exports = generateToken;
